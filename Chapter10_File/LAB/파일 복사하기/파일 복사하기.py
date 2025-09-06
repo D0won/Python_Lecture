2e35dd29b769b7884b2c03f53d4a2337288c179a
D0won <ldwlee03@gmail.com>
2025-09-07 00:55:13 +0900

    챕터 파일  Lab 추가 및 파일 처리

---
+++ Chapter10_File/LAB/파일 복사하기/파일 복사하기.py
@@ -0,0 +1,17 @@
+# 입력 파일과 출력 파일 이름을 받습니다.
+infilename = input("입력 파일 이름 : ")
+outfilename = input("출력 파일 이름 : ")
+
+#입력과 출력을 위한 파일을 엽니다.
+infile = open(infilename, "r")
+outfile = open(outfilename, "w")
+
+# 전체 파일을 읽습니다.
+s = infile.read()
+
+# 전체 파일을 씁니다.
+outfile.write(s)
+
+# 파일을 닫습니다.
+infile.close()
+outfile.close()
